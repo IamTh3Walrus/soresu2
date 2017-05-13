81ffd5579237a9d641d6d50375e8ed55e1cafcb1
Stephen Doe <Stephen Doe>
2017-05-13 14:44:02 -0400

    initial commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //database: 'mongodb://localhost:27017/soresu', //development
+  database: 'mongodb://StephenDoe:MoffTarkin@ds161960.mlab.com:61960/soresu', //production
+  secret: 'yoursecret'
+}
